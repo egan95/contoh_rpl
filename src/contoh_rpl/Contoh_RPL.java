8c4f6a51ef3b9fc9c544735e6f4891347eafd9b2
egan95 <egantkj@gmail.com>
2014-12-09 13:35:39 +0700

    commit dari baris 19

--- src/contoh_rpl/Contoh_RPL.java
+++ src/contoh_rpl/Contoh_RPL.java
@@ -16,6 +16,7 @@ public class Contoh_RPL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ok siap gan!!!");
     }
     
 }
